b28c3ea5876d90bce27fb2b264f4c6038c478794
guillaumebeysson <guillaume.beysson@gmail.com>
2020-10-29 15:27:14 +0100

    tableau

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
+
+var legumes = ['aubergine', 'courgette', 'choux', 'butternut', 'poivron', 'haricot', 'potiron', 'carotte', 'poireau', 'tomate', 'patate']
+console.log(legumes[0])
+console.log(legumes[10])
